8dae68d13a7bbd369e33f9e85a4337c9267e3edf
Marcelo Freri <marcelo.freri@gmail.com>
2020-11-03 01:31:11 -0300

    Added a tool to manually set the highscore.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Valentino
 PC version of my other game: Valentino for Android.
+
+## vale.cfg
+This file contains the highscore. Just replace the numeric value with 0 (zero) to start again, or use test/test.py to set any highscore you wish.
--- tools/score.py
+++ tools/score.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-with open('vale.cfg', 'r') as f:
+with open('../vale.cfg', 'r') as f:
 	data = f.read()
 print("Highscore: {}".format(data.split()[-1]))
 
-new_hs = int(input("New highscore: "))
-with open('vale.cfg', 'w') as f:
+new_hs = int(input("Enter new highscore: "))
+with open('../vale.cfg', 'w') as f:
 	f.write("Highscore = {}\n".format(new_hs))
+print("Value changed.")
